cad2bd821b720b3a94d9958e084daec7f224a8ca
Malith Anjana <malithanjana97@gmail.com>
2024-09-30 21:09:38 +0530

    create user authentication using clerk

--- client/components/shared/Header.tsx
+++ client/components/shared/Header.tsx
@@ -1,14 +1,26 @@
+import { SignedOut, SignedIn, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { Button } from '../ui/button'
 
 const Header = () => {
   return (
     <header className='w-full border-b'>
-        <div className="wrappe flex items-center justify-between">
+        <div className="wrapper flex items-center justify-between">
             <Link href='/'className='w-36'>
             <Image src='assets/images/logo.svg' width={128} height={38} alt='evently-logo'/>
             </Link>
+            <div className="flex w-32 justify-end gap-3">
+               <SignedIn>
+                <UserButton afterSignOutUrl='/'/>
+               </SignedIn>
+                <SignedOut>
+                    <Button asChild className='rounded-full' size='lg'>
+                        <Link href='/sign-in'>Login</Link>
+                    </Button>
+                </SignedOut>
+            </div>
         </div>
     </header>
   )
