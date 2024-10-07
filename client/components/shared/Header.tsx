47543c56c582842c09c42b3ca8498efe18c4dc98
Malith Anjana <malithanjana97@gmail.com>
2024-10-07 16:56:05 +0530

    fix the UI issue

--- client/components/shared/Header.tsx
+++ client/components/shared/Header.tsx
@@ -13,12 +13,12 @@ const Header = () => {
             <Link href='/'className='w-36'>
             <Image src='assets/images/logo.svg' width={128} height={38} alt='evently-logo'/>
             </Link>
-            <div className="flex w-32 justify-end gap-3">
-               <SignedIn>
+            <SignedIn>
                 <nav className='md:flex-between hidden w-full max-w-xs'>
                     <NavItems/>
                 </nav>
                </SignedIn>
+            <div className="flex w-32 justify-end gap-3">
                <SignedIn>
                 <UserButton afterSignOutUrl='/'/>
                 <MobileNav/>
