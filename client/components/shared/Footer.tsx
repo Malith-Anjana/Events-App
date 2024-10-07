8b344b5a939a02a253b433a51cbab3a8e390348a
Malith Anjana <malithanjana97@gmail.com>
2024-10-07 17:49:24 +0530

    build the footer

--- client/components/shared/Footer.tsx
+++ client/components/shared/Footer.tsx
@@ -1,8 +1,20 @@
+import Link  from 'next/link'
+import Image from 'next/image'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="border-t">
+      <div className='flex-center flex-between wrapper flex flex-col gap-4 p-5 text-center sm:flex-row'>
+        <Link href='/'>
+        <Image 
+        src='/assets/images/logo.svg' 
+        width={128} 
+        height={38} alt='evently-logo'/>
+        </Link>
+        <p>2024 Evently. All rights reserved.</p>
+      </div>
+    </footer>
   )
 }
 
