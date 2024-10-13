543f9151509d4fd1a2874666fb8d2085221f254c
Malith Anjana <malithanjana97@gmail.com>
2024-10-13 16:01:22 +0530

    implement the event details page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+   images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',  // Add the hostname here
+        port: '',  // Leave this empty unless you're using a specific port
+        pathname: '/f/**',  // Use wildcards to specify which paths are allowed (e.g., all images under /f/)
+      },
+    ],
+  },
+};
 
 export default nextConfig;
